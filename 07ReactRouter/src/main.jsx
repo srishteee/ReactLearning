b717c2ddfa2281922b2610da7c38e022fdb01e11
srishteee <srishtinagarkoti.25@gmail.com>
2024-09-19 16:38:17 +0530

    reactRouter

---
+++ 07ReactRouter/src/main.jsx
@@ -0,0 +1,23 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import Layout from './Layout.jsx'
+import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from 'react-router-dom'
+import Home from './components/Home/Home.jsx'
+import About from './components/About/About.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route  path='/' element={<Layout/>}>
+   <Route path="" element={<Home/>}/>
+   <Route path='about' element={<About/>}/>
+    </Route>
+  )
+) 
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <RouterProvider router={router}/>
+  </StrictMode>
+)
